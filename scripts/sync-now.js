533591d6d9008b83246571b3e62d799982c10fe8
naramarham <naramarham@gmail.com>
2026-09-17 09:22:17 +0500

    Delete scripts directory

--- scripts/sync-now.js
+++
@@ -1,5 +0,0 @@
-const { runDailySync } = require('../src/pipeline/runSync');
-
-runDailySync()
-  .then((stats) => { console.log('Sync finished:', stats); process.exit(0); })
-  .catch((e) => { console.error('Sync failed:', e); process.exit(1); });
